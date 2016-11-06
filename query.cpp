f745338c2b4d829091af9616a1649d5f4378a20f
cs16m037 <cs16m037@beeman.cse.iitm.ac.in>
2016-11-06 14:44:15 +0530

    written logic for searching, need to complete coding

--- query.cpp
+++ query.cpp
@@ -1,4 +1,5 @@
 #include "query.h"
+#include <iostream>
 Entry * BestFeature(Q_Heap *H, map<int, Graph *> *C );
 float score(Entry *node) {  
    
@@ -54,9 +55,29 @@ vector<int> *search(DGTreeNode *root, Graph *Q) {
     while(!C->empty()){
        q = BestFeature(H,C);
 
-       if(q == NULL){} // analyse carefully and complete
+       if(q == NULL){
+         //shouldn't come here --> indicates missing links/graphs in the index
+         cout<<"The index didn't cover all data graphs\n";
+         exit(1);
+       } // analyse carefully and complete
        else {
-          
+         DGTreeNode *g = q->treenode;
+         /*for(every g_plus in g->children){ //iterate over g->children
+           if (g_plus has no children) // if(g_plus->children.size() ==0){
+             if(grow_edge == NULL) {//check for NULL grow_edge
+                //A = A U gplus_S (The graph already matched its parent, with NULL grow_edge, it definitely matches this, so add to answer set
+             } 
+             else { //There is an edge to grow
+               //check to extend each match; iterate over all matches in q
+               //similar to FeatureExpansion logic
+               //... paper
+             } //end if-else of there is an edge to grow
+           } 
+           else { //g_plus has children
+             FeatureExpansion(Q,q,g_plus,H,C); //Put all children on heap with scores
+           }
+
+         }*/ //end iterate over all children
        }
        
     }
@@ -68,6 +89,36 @@ bool compareMapEntryNodes(const pair<int, Graph *>& p1,const pair<int, Graph *>&
     return p1.first < p2.first;
 }
 
+void FeatureExpansion(Graph *Q, Entry *q, DGTreeNode *g_plus, Q_Heap *H, map<int, Graph *> *C){
+   Entry *q_plus = new Entry;
+   q_plus->treenode = g_plus;
+   //TO_DO: q_plus->S_star = g_plus->S intersection C;
+   q_plus->matches = NULL;
+   if(g_plus->grow_edge == NULL){ //The map in parent is sufficient
+      //add to answer set or return NULL; will it come here?
+      //this code cannot be reached since grow_edge is NULL only in leaf nodes, which gets checked in the if part, else part calls this FeatureExpansion function.
+   }
+   else {
+      int ui = g_plus->grow_edge->x;
+      int uj = g_plus->grow_edge->y;
+      int valence = g_plus->grow_edge->valence;
+      
+      /*for(each match that has the correct ui label){ //iterate over all matches, if ui label is incorrect, continue
+         if(g_plus->edge_type == OPEN){
+            //for every neighbour v of f(ui) in Q {
+                if label of v matches label of uj {
+                   add this match to q_plus->matches
+                }
+            }
+         } //end edge_type OPEN case
+         else { // edge_type == CLOSE
+            //paper
+         }
+     } // end for each valid match*/
+   }
+   
+  //paper  if-else same 
+}
 Entry * BestFeature(Q_Heap *H, map<int, Graph *> *C ){
     Entry *q = new Entry;
     q = H->top();
@@ -79,13 +130,13 @@ Entry * BestFeature(Q_Heap *H, map<int, Graph *> *C ){
        delete q->S_star;
        q->S_star = new_S_star;
 
-       H->pop();
+       H->pop(); //possibly causes segmentation fault on empty H
        if (!q->S_star->empty()) {
             q->score = score(q);
 	    H->push(q);
        }
 
-       q = H->top();
+       q = H->top(); //possibly causes segmentation fault on empty H
 
     } // end while g+.S* is not included in C 
 
